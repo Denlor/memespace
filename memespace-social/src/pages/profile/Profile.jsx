3a0703d5b0102c102d263bc92f01a3cef77ec928
Victor Cueto <highlordvictor2014@gmail.com>
2021-12-27 22:19:26 -0500

    profile page layout

---
+++ memespace-social/src/pages/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Topbar from "../../components/topbar/Topbar"
+import "./profile.css"
+
+export default function Profile() {
+    return (
+        <>
+            <Topbar />
+            <div className="profile">
+                <Sidebar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img src="assets/post/1.jpg" alt="" className="profileCoverImg" />
+                            <img src="assets/person/1.webp" alt="" className="profileUserImg" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Jhonny Silverhand</h4>
+                            <h4 className="profileInfoDesc">hello there</h4>
+                        </div>
+                        
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <Rightbar profile/>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
