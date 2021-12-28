30ca846516a19b785fc5d8bb87b4cc67331c4553
Victor Cueto <highlordvictor2014@gmail.com>
2021-12-27 22:54:09 -0500

    login register layout

--- memespace-social/src/App.js
+++ memespace-social/src/App.js
@@ -1,8 +1,8 @@
 import Home from "./pages/home/Home"
-import Profile from "./pages/profile/Profile"
+
 
 function App() {
-  return <Profile />
+  return <Home />
 }
 
 export default App
--- memespace-social/src/components/rightbar/Rightbar.jsx
+++ memespace-social/src/components/rightbar/Rightbar.jsx
@@ -75,7 +75,7 @@ export default function Rightbar({profile}) {
     return (
         <div className="rightbar">
             <div className="rightbarWrapper">
-                <ProfileRightbar/>
+                {profile ? <ProfileRightbar/> : <HomeRightbar/>}
             </div>
         </div>
     )
